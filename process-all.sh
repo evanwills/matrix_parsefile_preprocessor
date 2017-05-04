9a658172f74411926bfba7f927cf4124efbb16cf
Evan Wills <evan.wills@acu.edu.au>
2017-05-04 15:25:40 +1000

    added cli script for processing all available parsefiles with a single call.

---
+++ process-all.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+echo ; echo ; echo ; echo ; echo ;
+
+php -f parse-file_process.cli.php parse-files/*.xml brief compare
